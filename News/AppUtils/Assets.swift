94b39d5a3c90bde3d830d0ca25d1bd18fae1ce46
Daniel <daniel.oliveira@foursys.com.br>
2022-10-25 09:47:25 -0300

    refactored the code

--- News/AppUtils/Assets.swift
+++ News/AppUtils/Assets.swift
@@ -34,5 +34,11 @@ struct Assets {
         static let cloudDrizzle = UIImage(systemName: "cloud.drizzle.fill")
         static let cloud = UIImage(systemName: "cloud.fill")
         static let cloudHail = UIImage(systemName: "cloud.hail.fill")
+        
+        static let wheater_0 = UIImage(named: "Wheater_0")
+        static let wheater_1 = UIImage(named: "Wheater_1")
+        static let wheater_2 = UIImage(named: "Wheater_2")
+        static let wheater_3 = UIImage(named: "Wheater_3")
+        static let wheater_4 = UIImage(named: "Wheater_4")
     }
 }
--- News/Wheater/WheaterView.swift
+++ News/Wheater/WheaterView.swift
@@ -14,45 +14,31 @@ class WheaterView: UIViewController {
     
     private let topStack: UIStackView = {
         
-        let imageView = Create.imageView(Assets.Images.cloudSun,
-                                         color: Assets.Colors.reverseDark)
-        let label = Create.label("70°",
-                                 font: Assets.font(60),
-                                 alignment: .center)
-        
-        let topStack = UIStackView(arrangedSubviews: [imageView, label])
+        let topStack = UIStackView()
         topStack.axis = .vertical
-        
-        imageView.shape(height: 120)
         
         return topStack
     }()
     
     private let daysStack: UIStackView = {
         
-        let createStack = {(wheater: UIImage?,
-                            temperature: Int) -> UIStackView in
-            
-            let imageViews = [Create.imageView(wheater,
-                                               color: Assets.Colors.reverseDark)]
-            let labels = [Create.label("\(temperature)°",
-                                      font: Assets.font(40),
-                                      alignment: .center)]
-            
-            let stack = UIStackView(arrangedSubviews: imageViews + labels)
-            stack.axis = .vertical
-            
-            return stack
-        }
-        
-        let daysStack = UIStackView(arrangedSubviews: [createStack(Assets.Images.cloudSun, 70),
-                                                       createStack(Assets.Images.cloud, 23),
-                                                       createStack(Assets.Images.cloudDrizzle, 65),
-                                                       createStack(Assets.Images.cloudSunBolt, 50),
-                                                       createStack(Assets.Images.sunMax, 32)])
+        let daysStack = UIStackView()
         daysStack.distribution = .fillEqually
         
         return daysStack
+    }()
+    
+    private let bottomStack: UIStackView = {
+        
+        let button = Create.button("Toca aqui") {_ in
+            
+            print("Toma!!")
+        }
+        
+        let bottomStack = UIStackView(arrangedSubviews: [button])
+        bottomStack.distribution = .fillEqually
+        
+        return bottomStack
     }()
     
     override func viewDidLoad() {
@@ -61,8 +47,16 @@ class WheaterView: UIViewController {
         
         title = "Wheater"
         
-        view.addSubviews([label, topStack, daysStack])
+        view.addSubviews([label, topStack, daysStack, bottomStack])
         view.backgroundColor = .systemRed
+        
+        topStack.addArrangedSubviews([createStack(Assets.Images.cloudSun, 70)])
+        
+        daysStack.addArrangedSubviews([createStack(day: "TUE", Assets.Images.cloudSun, 70),
+                                       createStack(day: "WED", Assets.Images.cloud, 23),
+                                       createStack(day: "THU", Assets.Images.cloudDrizzle, 65),
+                                       createStack(day: "FRI", Assets.Images.cloudSunBolt, 50),
+                                       createStack(day: "SAT", Assets.Images.sunMax, 32)])
         
         label.constraint(to: view.safeAreaLayoutGuide, [.centerX: 0,.top: 50])
         label.constraint(by: [.width], multiplier: 0.7)
@@ -73,6 +67,42 @@ class WheaterView: UIViewController {
         daysStack.constraint(by: [.leading,.trailing])
         daysStack.constraint(to: topStack, with: [.top:.bottom], constant: 35)
         
-        daysStack.shape(height: 120)
+        bottomStack.constraint(to: daysStack, with: [.top:.bottom])
+        bottomStack.constraint(by: [.leading,.trailing,.bottom])
+    }
+    
+    private func createStack(day: String? = nil,
+                             _ wheater: UIImage?,
+                             _  temperature: Int) -> UIStackView {
+        
+        let imageView = Create.imageView(wheater,
+                                         color: Assets.Colors.reverseDark)
+        let label = Create.label("\(temperature)°",
+                                 alignment: .center)
+        
+        if let day = day {
+            
+            label.font = Assets.font(40)
+            
+            let dayLabel = Create.label(day,
+                                        font: Assets.font(20),
+                                        alignment: .center)
+            
+            let stack = UIStackView(arrangedSubviews: [dayLabel, imageView, label])
+            stack.axis = .vertical
+            
+            imageView.constraint(by: [.height], multiplier: 0.5)
+            
+            return stack
+        }
+        
+        label.font = Assets.font(60)
+        
+        let stack = UIStackView(arrangedSubviews: [imageView, label])
+        stack.axis = .vertical
+        
+        imageView.constraint(by: [.height], multiplier: 0.65)
+        
+        return stack
     }
 }
