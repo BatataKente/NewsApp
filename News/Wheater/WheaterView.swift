203d7f0f9d06667316570bce0baf79a97e91ed1a
Daniel <daniel.oliveira@foursys.com.br>
2022-10-25 10:42:10 -0300

    refactored the code

--- News/Wheater/WheaterView.swift
+++ News/Wheater/WheaterView.swift
@@ -47,8 +47,8 @@ class WheaterView: UIViewController {
         
         title = "Wheater"
         
-        view.addSubviews([label, topStack, daysStack, bottomStack])
         view.backgroundColor = .systemRed
+        view.addSubviews([label, topStack, daysStack, bottomStack])
         
         topStack.addArrangedSubviews([createStack(Assets.Images.cloudSun, 70)])
         
@@ -58,17 +58,42 @@ class WheaterView: UIViewController {
                                        createStack(day: "FRI", Assets.Images.cloudSunBolt, 50),
                                        createStack(day: "SAT", Assets.Images.sunMax, 32)])
         
-        label.constraint(to: view.safeAreaLayoutGuide, [.centerX: 0,.top: 50])
-        label.constraint(by: [.width], multiplier: 0.7)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        topStack.translatesAutoresizingMaskIntoConstraints = false
+        daysStack.translatesAutoresizingMaskIntoConstraints = false
+        bottomStack.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+
+            label.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            label.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.7),
+
+            topStack.leadingAnchor.constraint(equalTo: label.leadingAnchor),
+            topStack.trailingAnchor.constraint(equalTo: label.trailingAnchor),
+            topStack.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 35),
+
+            daysStack.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            daysStack.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            daysStack.topAnchor.constraint(equalTo: topStack.bottomAnchor, constant: 35),
+
+            bottomStack.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomStack.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bottomStack.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomStack.topAnchor.constraint(equalTo: daysStack.bottomAnchor)
+        ])
         
-        topStack.constraint(to: label, by: [.leading,.trailing])
-        topStack.constraint(to: label, with: [.top:.bottom], constant: 35)
-        
-        daysStack.constraint(by: [.leading,.trailing])
-        daysStack.constraint(to: topStack, with: [.top:.bottom], constant: 35)
-        
-        bottomStack.constraint(to: daysStack, with: [.top:.bottom])
-        bottomStack.constraint(by: [.leading,.trailing,.bottom])
+//        label.constraint(to: view.safeAreaLayoutGuide, [.centerX: 0,.top: 50])
+//        label.constraint(by: [.width], multiplier: 0.7)
+//
+//        topStack.constraint(to: label, by: [.leading,.trailing])
+//        topStack.constraint(to: label, with: [.top:.bottom], constant: 35)
+//
+//        daysStack.constraint(by: [.leading,.trailing])
+//        daysStack.constraint(to: topStack, with: [.top:.bottom], constant: 35)
+//
+//        bottomStack.constraint(to: daysStack, with: [.top:.bottom])
+//        bottomStack.constraint(by: [.leading,.trailing,.bottom])
     }
     
     private func createStack(day: String? = nil,
